bfc09cf65b0428d4825990398e53763b761ed0f3
Luis Carlos Carrillo <luis.carrillo@octapus.io>
2026-03-22 11:56:29 -0500

    Simple data

---
+++ data-types/simple-data.py
@@ -0,0 +1,23 @@
+#Textos o cadenas de una sola línea
+"String "
+'String'
+
+#Textos o cadenaas de mas de una línea
+"""String 
+de
+mas de 
+una línea"""
+
+'''String 
+de
+mas de 
+una línea'''
+
+#Datos de tipo númerico: Números enteros, aunque no es necesario pasar el tipo de dato, python lo entiende
+int = 40
+#Números flotantes, o con. reales
+float = 40.1
+
+#Booleanos con dos posibilidades
+True
+False
