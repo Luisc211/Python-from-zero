130dc5eab4e4ceddbb99d4de34f7ef079d761971
Luis Carlos Carrillo <luis.carrillo@octapus.io>
2026-03-24 18:36:24 -0500

    Arithmetic operators

---
+++ arithmetic-operators/arithmetic_operators.py
@@ -0,0 +1,24 @@
+#Suma y resta (+ y -)
+suma = 12+5
+resta = 12-5
+
+print(suma)
+print(resta)
+
+#Multiplicación y división (* y /)
+multiplicacion = 10*5
+division = 12/5 #devuelve un dato flotante o float
+print(multiplicacion)
+print(division)
+
+#Potenciación exponente (**)
+exponente = 12 ** 5
+print(exponente)
+
+#División baja (//), devuelve entero redondeado hacia abajo 
+division_baja = 12 // 5 
+print(division_baja)
+
+#Resto o modulo
+resto = 12 % 5
+print(resto)
