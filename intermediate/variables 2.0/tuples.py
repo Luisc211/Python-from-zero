d379cb90268d3b5899e5cc0a882e4efeb12b2e66
Luis Carlos Carrillo <luis.carrillo@octapus.io>
2026-04-15 13:09:45 -0500

    Tuples

---
+++ intermediate/variables 2.0/tuples.py
@@ -0,0 +1,9 @@
+#Función que nos permite crear una tupla con tuple(), recibe como unico parámetro una lista, la cual va a convertir en tupla
+tupla = tuple(['Dato 01', 'Dato 02'])
+#Tupla sin parentesis con multiples datos
+#!No hace falta la coma al final, es decir después del ultimo dato
+simple_tuple = "Dato 01", "Dato 02"
+#Tupla sin parentesis de un solo dato
+#!Debemos darle la coma al final, ya que si no lo hacemos es como si estuvieramos creando un string 
+#Se recomienda utilizar tuplas cuando son datos de solo lectura, o que no se tienden a modificar
+simple_tuple = "Unico dato",
