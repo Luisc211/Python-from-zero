d1ee8b0c8afb9e4f570ee0781639cb9506f1eafe
Luis Carlos Carrillo <luis.carrillo@octapus.io>
2026-04-19 10:46:04 -0500

    Dictionary 2.0

---
+++ intermediate/dictionarys 2.0/dictionarys.py
@@ -0,0 +1,2 @@
+#Creando diccionarios con dict(), pide como parámetros key=value, las funciones tuple, dict, list nos permiten crear los arrays vacios
+dictionary = dict(name="Luis", last_name="Carrillo")
