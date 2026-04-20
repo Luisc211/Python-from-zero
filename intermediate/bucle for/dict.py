eaf9007b27d7a2f3a0ded062d6fb63df4ccd9f88
Luis Carlos Carrillo <luis.carrillo@octapus.io>
2026-04-20 18:54:00 -0500

    Touring a dictionary with for

---
+++ intermediate/bucle for/dict.py
@@ -0,0 +1,11 @@
+#Recorriendo un diccionario
+
+dictionary = {
+    'name': 'Luis Carlos',
+    'last_name': 'Carrillo',
+    'age': 31
+}
+
+#Dveuelve par clave valor
+for key in dictionary.items():
+    print(key)
