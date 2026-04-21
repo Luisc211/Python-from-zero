839f57e675fd03d463b26d225e3ca624a8bb213c
Luis Carlos Carrillo <luis.carrillo@octapus.io>
2026-04-21 11:25:35 -0500

    Bucle while

---
+++ intermediate/bucle while/bucle_while.py
@@ -0,0 +1,9 @@
+# Bucle while: a diferencia del for que se usa mas para iterar listas, o arrays, el while se va a ejecutar mientras una condicion sea verdadera, cuando la condición deje de ser verdadera, el bucle terminara, y el programa continuara con las demás instrucciones.
+contador = 0
+
+#Se ejecutara mientras el contador sea menor a 5, cuando deje de cumplirse la condición dejara de ejecutarse y seguira con las demás instrucciones
+while contador < 5:
+    contador += 1
+    print(contador)
+
+print("El bucle mientras, o while a llegado a su final.")
